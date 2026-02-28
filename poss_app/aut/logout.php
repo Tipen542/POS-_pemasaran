bdbb5f691a237330cb81bdca4d20886d1027b7ec
Tipen542 <grardgrard1@gmail.com>
2026-03-01 02:51:09 +0700

    Delete poss_app directory

--- poss_app/aut/logout.php
+++
@@ -1,5 +0,0 @@
-<?php
-include '../config/database.php';
-session_destroy();
-header("Location: ../index.php");
-?>
--- poss_app/sales_delate.php
+++
@@ -1,6 +0,0 @@
-<?php
-include 'config/database.php';
-$id = $_GET['id'];
-$conn->query("DELETE FROM sales WHERE id=$id");
-header("Location: sales.php");
-?>
